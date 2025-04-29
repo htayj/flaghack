1fff655532402bfc0b2a1086303452e71e442558
Taylor Hardy <hardy.taylor.j@gmail.com>
2025-04-28 20:52:37 -0400

    remove some gitignored files

--- packages/server/build/dts/Api.d.ts
+++
@@ -1,4 +0,0 @@
-import { Layer } from "effect";
-import { TodosRepository } from "./TodosRepository.js";
-export declare const ApiLive: Layer.Layer<import("@effect/platform/HttpApi").Api, never, TodosRepository>;
-//# sourceMappingURL=Api.d.ts.map
